0b79ea0b620347e73f986788704cb1f12239ca54
231840276-Yang Haoyu <2983075284@qq.com>
2025-02-07 17:55:27 +0800

    finish keyboard

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+    uint32_t key_data = inl(KBD_ADDR);
+    kbd->keycode = key_data & (~KEYDOWN_MASK) ;
+    kbd->keydown = ((kbd->keycode | KEYDOWN_MASK) == key_data) ? true : false;
 }
